dbc5e8ac49d2cdb5aab5f003dc3b80863b1dccc1
Kyri IOULIANOU <kioulian@e5r5p8.jnb.42.fr>
2016-10-04 18:21:29 +0200

    Started css, started index

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'].'config/database.php');
-	    
+	$root = $_SERVER['DOCUMENT_ROOT'];
+	require_once($root.'/config/database.php');
+	$page_title = "Home";
 
+	//require_once($root.'/views/header.php');
+
+	if (isset($_GET['controller']) && isset($_GET['action']))
+	{
+		$controller = $_GET['controller'];
+		$action = $_GET['action'];
+	} else {
+		$controller = 'pages';
+		$action = 'home';
+	}
  ?>
---
+++ views/header.php
@@ -0,0 +1,12 @@
+<?php
+
+	header("Content-Type: text/html");
+	session_start();
+
+?>
+
+<html>
+<head>
+	<title><?php echo $page_title?></title>
+</head>
+<body>
