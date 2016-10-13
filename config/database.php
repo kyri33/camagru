3a2a4534847285064ed9a71c711eb3dcd5c7a761
Viashen GOVENDER <vgovende@e5r5p11.jnb.42.fr>
2016-10-13 09:51:32 +0200

    latest views update, button in upload fixed

--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
     $DB_HOST = '127.0.0.1';
     $DB_NAME = 'db_camagru';
--- views/upload/upload.php
+++ views/upload/upload.php
@@ -1,6 +1,4 @@
 <script type="application/javascript" src="js/upload.js"></script>
-<button id="play">Play</button>
-<button id="pause">Pause</button>
-<button id="stop">Stop</button>
-<br />
 <video id="camera" width="640" height="480"></video>
+<br/>
+<button id="capture">Capture</button>
