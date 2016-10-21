4f84e8351054a59a7d867cc677308cdceef0cff3
Viashen GOVENDER <vgovende@e5r3p11.jnb.42.fr>
2016-10-21 14:48:53 +0200

    Selectors came along

--- views/upload/upload.php
+++ views/upload/upload.php
@@ -1,23 +1,44 @@
 <script type="application/javascript" src="js/upload.js"></script>
 
+<div class="img">
+  <input type="radio" name="filter" id="check4"/>
+    <label id='colourful' for="check4"><img src="glasses.png" alt="Forest" width="300" height="200"></label>
+  </a>
+  <div class="desc">Filter Name</div>
+</div>
+
+<div class="img">
+  <input type="radio" name="filter" id="check1"/>
+    <label id='colourful' for="check1"><img src="glasses.png" alt="Forest" width="300" height="200"></label>
+  </a>
+  <div class="desc">Filter Name</div>
+</div>
+
+<div class="img">
+  <input type="radio" name="filter" id="check3"/>
+    <label id='colourful' for="check3"><img src="ben.png" alt="Forest" width="300" height="200"></label>
+  </a>
+  <div class="desc">Filter Name</div>
+</div>
+
+<div class="img">
+  <input type="radio" name="filter" id="check2"/>
+    <label id='colourful' for="check2"><img src="face.png" alt="Forest" width="300" height="200"></label>
+  </a>
+  <div class="desc">Filter Name</div>
+</div>
+
 
 <div class="img">
   <a target="_blank" href="">
-    <img src="imageglasses.png" alt="Fjords" width="300" height="200">
+    <img src="face.png" alt="Mountains" width="300" height="200">
   </a>
   <div class="desc">Filter Name</div>
 </div>
 
 <div class="img">
   <a target="_blank" href="">
-    <img src="glasses.png" alt="Forest" width="300" height="200">
-  </a>
-  <div class="desc">Filter Name</div>
-</div>
-
-<div class="img">
-  <a target="_blank" href="">
-    <img src="ben.png" alt="Northern Lights" width="300" height="200">
+    <img src="face.png" alt="Mountains" width="300" height="200">
   </a>
   <div class="desc">Filter Name</div>
 </div>
